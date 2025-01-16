fb3ef3ece1c8e71df81036c7d12687e5bbac5469
build-failure <michael@lewkowski.de>
2025-01-16 10:19:46 +0100

    Fix Docker SDK installation path issues

--- .devcontainer/features/spryker-docker-sdk/install.sh
+++ .devcontainer/features/spryker-docker-sdk/install.sh
@@ -1,30 +1,11 @@
 #!/bin/sh
 set -e
 
-WORKSPACE_FOLDER=${WORKSPACE_FOLDER:-/workspaces/my-project} # Default workspace folder
-SPRYKER_DOCKER_SDK_TARGET_FOLDER="$WORKSPACE_FOLDER/docker"  
-
-echo "Activating feature 'color'"
-echo "The provided favorite color is: ${FAVORITE}"
-
-
-# The 'install.sh' entrypoint script is always executed as the root user.
-#
-# These following environment variables are passed in by the dev container CLI.
-# These may be useful in instances where the context of the final 
-# remoteUser or containerUser is useful.
-# For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+echo "Create /opt directory if not exists"
+mkdir -p /opt
 
 echo "Cloning Spryker Docker SDK git repository $VERSION"
-git clone --branch $VERSION --depth 1 https://github.com/spryker/docker-sdk.git $SPRYKER_DOCKER_SDK_TARGET_FOLDER
+git clone --branch $VERSION --depth 1 https://github.com/spryker/docker-sdk.git /opt/spryker-docker-sdk
 
-echo "Initialize Docker Setup"
-#docker/sdk bootstrap # Disabled, expects Spryker project setup incl. deploy.yml 
-
-echo "Build and run Spryker application(s)"
-#docker/sdk up # Disabled, requires bootstrapping to be enabled
+echo "Set container user $_CONTAINER_USER as owner of Docker SDK"
+chown -R $_CONTAINER_USER /opt/spryker-docker-sdk
